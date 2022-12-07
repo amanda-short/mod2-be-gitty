c87bc36ba0af5a2ae0fb9216b9f56113c6c1a580
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2022-12-07 10:52:43 -0800

    end of work session push

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,7 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS github_users;
+DROP TABLE IF EXISTS posts;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -17,3 +18,9 @@ CREATE TABLE github_users (
   email TEXT,
   avatar TEXT
 );
+
+CREATE TABLE posts (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  body VARCHAR NOT NULL
+);
+
