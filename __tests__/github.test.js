fe19e6bcfe46b19ed4a7153d9b377a3d03aef67d
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2022-12-07 11:12:41 -0800

    CI4

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -19,19 +19,19 @@ describe('github route', () => {
     );
   });
 
-  it.skip('/api/v1/github/callback should redirect logged in users to dashboard', async () => {
-    const res = await request
-      .agent(app)
-      .get('/api/v1/github/callback?code=42')
-      .redirects(1);
-    expect(res.body).toEqual({
-      id: expect.any(String),
-      login: 'mock_github_user',
-      email: '1@1.com',
-      avatar:
-        'https://shatterme.fandom.com/wiki/Dog?file=ArtbreederGeneralLogo_.jpg',
-      iat: expect.any(Number),
-      exp: expect.any(Number),
-    });
-  });
+  // it.skip('/api/v1/github/callback should redirect logged in users to dashboard', async () => {
+  //   const res = await request
+  //     .agent(app)
+  //     .get('/api/v1/github/callback?code=42')
+  //     .redirects(1);
+  //   expect(res.body).toEqual({
+  //     id: expect.any(String),
+  //     login: 'mock_github_user',
+  //     email: '1@1.com',
+  //     avatar:
+  //       'https://shatterme.fandom.com/wiki/Dog?file=ArtbreederGeneralLogo_.jpg',
+  //     iat: expect.any(Number),
+  //     exp: expect.any(Number),
+  //   });
+  // });
 });
